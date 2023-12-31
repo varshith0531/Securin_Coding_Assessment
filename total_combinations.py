915d52428e68260e0326d223eecab953eeaf308c
Varshith Yadavalli <93373912+varshith0531@users.noreply.github.com>
2023-12-31 16:02:22 +0530

    total_combinations.py

--- total_combinations.py
+++ total_combinations.py
@@ -1,7 +1,5 @@
 """1. How many total combinations are possible? Show the math along with the code!"""
-sides_per_die = 6
-total_combinations = 0
-for i in range(1, sides_per_die + 1):
-  for j in range(1, sides_per_die + 1):
-    total_combinations += 1
-print("Total possible combinations:",total_combinations)
+faces_die_a = 6
+faces_die_b = 6
+total_combinations = faces_die_a * faces_die_b
+print("Total combinations:", total_combinations)
