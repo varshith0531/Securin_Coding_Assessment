520a427ca72582286795503120390e36695235c3
Varshith Yadavalli <93373912+varshith0531@users.noreply.github.com>
2023-12-31 15:43:06 +0530

    total_combinations.py

--- total_combinations.py
+++ total_combinations.py
@@ -1,4 +1,7 @@
 """1. How many total combinations are possible? Show the math along with the code!"""
-die_faces = list(map(int,input().split()))
-total_combinations = len(die_faces) * len(die_faces)
-print("Total combinations:", total_combinations)
+sides_per_die = 6
+total_combinations = 0
+for i in range(1, sides_per_die + 1):
+  for j in range(1, sides_per_die + 1):
+    total_combinations += 1
+print("Total possible combinations:",total_combinations)
